fa60cccccc751e701491852ae6b798e2237a3b0d
bnguien <nguyennguyendangbao8@gmail.com>
2026-02-12 12:34:21 +0700

    fix: update ALLOWED_HOSTS for local and production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-uy4&cfk30-_-7+q)^(p(o%c7s!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
